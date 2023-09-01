a4a628996fc7eaba0b517fd7ce0d94f80a6cb519
haris-rafique24 <62500642+haris-rafique24@users.noreply.github.com>
2023-09-01 10:12:07 +0500

    Add files via upload

---
+++ src/Form.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom'
+
+interface State{
+    name:String
+}
+interface Props{
+
+}
+export class Form extends React.Component<Props, State> {
+    constructor(props: Props){
+        super(props);
+        this.state={
+            name:''
+        }
+    }
+    handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        let input = ReactDOM.findDOMNode(this.refs["name"]) as HTMLInputElement;    
+        console.log(input.value)
+        this.setState({
+            name:input.value
+        })
+    }
+
+
+    render() {
+        return <form className="commentForm" onSubmit={ e => this.handleSubmit(e) }>
+                 <input type="text" placeholder="Your name" ref="name" />
+                 <button type="submit" >Submit</button>
+                 {this.state.name}
+               </form>;
+    }
+}
